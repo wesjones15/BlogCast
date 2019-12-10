d857563df8678685987da5fb20c653119fc2cc94
wes <wes.jones1597@gmail.com>
2019-12-10 11:45:36 -0500

    uml progress

--- src/main/java/blogcast/models/Blog.java
+++ src/main/java/blogcast/models/Blog.java
@@ -7,7 +7,7 @@ public class Blog {
 
     private List<User> authorizedUsers = new ArrayList<>();
 
-    //private List<Comment> blogComments = new ArrayList<>();
+    private List<Comment> blogComments = new ArrayList<>();
 
     public Boolean isAuthorized(User user){
         return authorizedUsers.contains(user);
@@ -19,3 +19,4 @@ public class Blog {
 
 
 }
+//all comments key, vlaues are a list of key
--- src/main/java/blogcast/models/User.java
+++ src/main/java/blogcast/models/User.java
@@ -34,3 +34,5 @@ public class User {
     }
 
 }
+//blog specific
+//dependency injection?
