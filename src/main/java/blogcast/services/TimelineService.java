eebc04303dd69bc829002a58ef210102fc2aaedc
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-13 17:15:09 -0500

    made changes to timeline

--- src/main/java/blogcast/services/TimelineService.java
+++ src/main/java/blogcast/services/TimelineService.java
@@ -1,6 +1,19 @@
 package blogcast.services;
 
+import blogcast.repositories.TimelineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class TimelineService {
+
+    private TimelineRepository timelineRepository;
+    @Autowired
+    public TimelineService(TimelineRepository timelineRepository){
+        this.timelineRepository = timelineRepository;
+    }
+
      public Boolean removeBlog(){
 
         return false;
