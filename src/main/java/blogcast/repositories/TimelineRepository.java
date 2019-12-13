e8906d2cccebcfb2ea50339c4fbfef663053e0d9
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-13 16:24:51 -0500

    merge additions

--- src/main/java/blogcast/repositories/TimelineRepository.java
+++ src/main/java/blogcast/repositories/TimelineRepository.java
@@ -1,6 +1,8 @@
 package blogcast.repositories;
 
+import blogcast.models.Timeline;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TimelineRepository extends CrudRepository {
+public interface TimelineRepository extends CrudRepository<Timeline, Long> {
+
 }
