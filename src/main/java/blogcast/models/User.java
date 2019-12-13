fec4e185e0f0d4448ad06b0fcef67f9f4f2a9329
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-13 11:18:28 -0500

    user class

--- src/main/java/blogcast/models/User.java
+++ src/main/java/blogcast/models/User.java
@@ -1,42 +1,78 @@
 package blogcast.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class User {
 
-    private String userName;
-    List<User> followers = new ArrayList<>();
-    List<User> following = new ArrayList<>();
-    List<Blog> userBlogs = new ArrayList<>();
-    List<DirectMessage> dms = new ArrayList<>();
+    @Id
+    @GeneratedValue
+    @Column(name = "User_ID")
+    private Long id;
 
-    public String comment(){
-        return null;
+    @Column(name = "Followers")
+    private List<User> followers = new ArrayList<>();
+
+    @Column(name = "Following")
+    private List<User> following = new ArrayList<>();
+
+    @Column(name = "Blog_Entries")
+    private List<BlogEntry> userBlogEntries = new ArrayList<>();
+
+    @Column(name = "Direct_Messages")
+    private List<DirectMessage> directMessages = new ArrayList<>();
+
+    public void commentOnBlogEntry(BlogEntry blogEntry, Comment comment, String words){
+
+        comment.setComment(words);
+        blogEntry.comments.add(comment);
     }
 
-    public String post(){
+    //Method incomplete
+    //Need to figure out the "body Object"
+    //And tags
+    public String postBlogEntry(BlogEntry blogEntry, String title, Object body){
 
-        return null;
+        blogEntry.setTitle(title);
+        blogEntry.setBody(body);
+        userBlogEntries.add(blogEntry);
+
+        return blogEntry.toString();
     }
 
-    public Boolean follow(){
+    public Boolean follow(User user) throws Exception {
 
+        if (followers.contains(user)) {
+            throw new Exception("You already follow this user!wq");
+        }
+        else {
+            followers.add(user);
+            followers.contains(user);
+            return true;
+        }
+    }
+
+    public Boolean unFollow(User user){
+
+        if(followers.contains(user)){
+            followers.remove(user);
+            return true;
+        }
         return false;
     }
 
-    public Boolean unFollow(){
 
-        return false;
+    public Long getId() {
+        return id;
     }
 
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public List<User> getFollowers() {
@@ -55,13 +91,19 @@ public class User {
         this.following = following;
     }
 
-    public List<Blog> getUserBlogs() {
-        return userBlogs;
+    public List<BlogEntry> getUserBlogEntrys() {
+        return userBlogEntries;
     }
 
-    public void setUserBlogs(List<Blog> userBlogs) {
-        this.userBlogs = userBlogs;
+    public void setUserBlogEntrys(List<BlogEntry> userBlogEntrys) {
+        this.userBlogEntries = userBlogEntrys;
+    }
+
+    public List<DirectMessage> getDirectMessages() {
+        return directMessages;
+    }
+
+    public void setDirectMessages(List<DirectMessage> directMessages) {
+        this.directMessages = directMessages;
     }
 }
-//blog specific
-//dependency injection?
