a694bed1f0724960f1c09c7f0a6781c729748a28
MannyMonocle <eetumadu@gmail.com>
2019-12-13 16:30:32 -0500

    repo updated

--- src/main/java/blogcast/repositories/BlogRepository.java
+++ src/main/java/blogcast/repositories/BlogRepository.java
@@ -2,6 +2,8 @@ package blogcast.repositories;
 
 import blogcast.models.Blog;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends CrudRepository<Blog,Long> {
 }
