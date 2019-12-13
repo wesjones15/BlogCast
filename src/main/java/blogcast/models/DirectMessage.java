3776574cf4f13b98a073b0cec5c32e4e4d0c4558
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-13 14:05:09 -0500

    Remove methods from user and get/set for dm

--- src/main/java/blogcast/models/DirectMessage.java
+++ src/main/java/blogcast/models/DirectMessage.java
@@ -17,4 +17,52 @@ public class DirectMessage {
     private String title;
     private String message;
     private Long timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Long fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public Long getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(Long toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
