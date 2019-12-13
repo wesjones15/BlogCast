436c8469a244d897d609793047d527bb36de1321
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-13 15:09:16 -0500

    delete workspace

--- src/main/java/blogcast/models/Timeline.java
+++ src/main/java/blogcast/models/Timeline.java
@@ -9,11 +9,6 @@ import java.util.Stack;
 public class Timeline {
     private Long id;
 
-<<<<<<< HEAD
-    List<BlogEntry> AllBlogs = new ArrayList();
-=======
->>>>>>> 99eb4a5a96f1738492cae33c86cf3dcbe29691b9
-
    private List<BlogEntry> AllBlogs = new ArrayList<>();
 
 
