04674fdba8ea9a7db070443d38d5ead9ff3a63f1
MannyMonocle <eetumadu@gmail.com>
2019-12-13 14:35:00 -0500

    Blog POJO done

--- src/main/java/blogcast/models/Blog.java
+++ src/main/java/blogcast/models/Blog.java
@@ -13,22 +13,71 @@ public class Blog {
     @GeneratedValue
     private Long id;
     private String blogName;
-    private List<User> authorizedUsers;
-    private List<BlogEntry> blogs = new ArrayList<>();
+    private List<User> authorizedUsers = new ArrayList<>();
+    private List<BlogEntry> allPosts = new ArrayList<>();
     private List<User> followers = new ArrayList<>();
+    private List<Comment> blogComments = new ArrayList<>();
 
     //CONSTRUCTORS
+    public Blog(String blogName, User creator) {
+        this.blogName = blogName;
+        this.authorizedUsers.add(creator);
+    }
+
+    public Blog(String blogName, List<User> creators) {
+        this.blogName = blogName;
+        this.authorizedUsers = creators;
+    }
 
 
-    //GETTERS, SETTERS, AND SIMILAR METHODS
-    public Long getId(){ return this.id; }
-    public String getBlogName(){ return this.blogName; }
-    public void  setBlogName(String newName){ this.blogName = newName; }
+    //GETTERS AND SETTERS
 
+    public Long getId() {
+        return id;
+    }
 
-    //OTHER METHODS
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getBlogName() {
+        return blogName;
+    }
+
+    public void setBlogName(String blogName) {
+        this.blogName = blogName;
+    }
+
+    public List<User> getAuthorizedUsers() {
+        return authorizedUsers;
+    }
+
+    public void setAuthorizedUsers(List<User> authorizedUsers) {
+        this.authorizedUsers = authorizedUsers;
+    }
+
+    public List<BlogEntry> getAllPosts() {
+        return allPosts;
+    }
+
+    public void setAllPosts(List<BlogEntry> allPosts) {
+        this.allPosts = allPosts;
+    }
+
+    public List<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<User> followers) {
+        this.followers = followers;
+    }
+
+    public List<Comment> getBlogComments() {
+        return blogComments;
+    }
+
+    public void setBlogComments(List<Comment> blogComments) {
+        this.blogComments = blogComments;
+    }
 }
 
-//    FUTURE STUFF TO ADD:
-//    private List<Comment> blogComments = new ArrayList<>();
