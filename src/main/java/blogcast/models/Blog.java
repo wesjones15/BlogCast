423e399e77612b74ae081827056f889cdfab9749
MannyMonocle <eetumadu@gmail.com>
2019-12-13 13:20:54 -0500

    save

--- src/main/java/blogcast/models/Blog.java
+++ src/main/java/blogcast/models/Blog.java
@@ -1,10 +1,16 @@
 package blogcast.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Blog {
     //FIELDS
+    @Id
+    @GeneratedValue
     private Long id;
     private String blogName;
     private List<User> authorizedUsers;
@@ -14,7 +20,10 @@ public class Blog {
     //CONSTRUCTORS
 
 
-    //GETTERS AND SETTERS
+    //GETTERS, SETTERS, AND SIMILAR METHODS
+    public Long getId(){ return this.id; }
+    public String getBlogName(){ return this.blogName; }
+    public void  setBlogName(String newName){ this.blogName = newName; }
 
 
     //OTHER METHODS
